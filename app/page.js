216705bc1d0a6138247f66dcadfd341e3ffa5dc2
Gustavo S4ntos <gustavo.moreira.gov@hotmail.com>
2023-08-03 19:44:28 -0300

    :up:

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 "use client";
 
 import HomePage from "@/components/home";
+import { CenterBtn } from "@/components/custom/btns";
 
 export default function Home() {
 
   return (
     <>
       <HomePage />
+      <CenterBtn conteudo={'Outros Generos'} rota={'/generos'} />
     </>
   );
 }
--- components/home/index.jsx
+++ components/home/index.jsx
@@ -6,7 +6,6 @@ import Cards from "@/components/home/cards";
 import Ficcao from "@/components/home/generos/ficcao";
 import Aventura from "@/components/home/generos/aventura";
 import Historia from "@/components/home/generos/historia";
-import { CenterBtn } from "@/components/custom/btns";
 import Modal from "../custom/modal";
 
 const HomePage = () => {
@@ -21,7 +20,6 @@ const HomePage = () => {
             <Ficcao />
             <Aventura click={() => setModal(!modal)} />
             <Historia />
-            <CenterBtn conteudo={'Outros Generos'} rota={'/generos'} />
             {modal && ( <Modal click={() => setModal(!modal)} /> )}
         </>
     )
