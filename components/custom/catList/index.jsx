0db22e90f1c93bbc8602e2ed9cce4898b0ff6fc1
Gustavo S4ntos <gustavo.moreira.gov@hotmail.com>
2023-08-03 16:30:19 -0300

    :package:

--- components/custom/catList/index.jsx
+++ components/custom/catList/index.jsx
@@ -1,15 +1,21 @@
-import React from "react";
-import { Autoplay } from "swiper";
+import React, { useState, useEffect } from "react";
 import { Swiper } from "swiper/react";
+import { Autoplay } from "swiper";
 import style from './style.module.css';
 
 const CatList = ({ categoria, children }) => {
+
+    const [ ajuste, setAjuste ] = useState(6);
+
+    useEffect(() => {
+        setAjuste(() => window.innerWidth >= 1193 ? 6 : 4 && window.innerWidth > 606 ? 3 : 3);
+    },[]);
 
     return (
         <>
             <section className={style['f-container']}>
                 <h3>{categoria}</h3>
-                <Swiper loop={true} spaceBetween={15} slidesPerView={6} autoplay={{ delay: 8000 }}
+                <Swiper loop={true} spaceBetween={15} slidesPerView={ajuste} autoplay={{ delay: 8000 }}
                     modules={[Autoplay]} className="mySwiper"
                 >
                     {children}
